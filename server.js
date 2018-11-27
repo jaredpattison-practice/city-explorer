dd2818e46fd3396e206f11600063ddecfe735dea
jared <jaredpattison@users.noreply.github.com>
2018-11-26 18:52:33 -0800

    fixed links, meetup api working

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.get('/movies', getMovies);
 
 app.get('/trails', getTrails);
 
+app.get('/meetups', getMeetup);
+
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
@@ -56,6 +58,14 @@ function Yelp(business) {
   this.image_url = business.image_url;
   this.rating = business.rating;
   this.price = business.price;
+  this.url = business.url;
+}
+
+function Meetup(event) {
+  this.link = event.link;
+  this.name = event.name;
+  this.host = event.venue.name;
+  this.creation_date = new Date(event.created * 1000).toString().slice(0,10);
 }
 
 function Trails(trail) {
@@ -68,6 +78,7 @@ function Trails(trail) {
   this.stars = trail.stars;
   this.star_votes = trail.star_votes;
   this.summary = trail.summary;
+  this.trail_url = trail.url;
 }
 
 function Movie(movie) {
@@ -119,12 +130,12 @@ function getMovies(request, response) {
 }
 
 function getTrails(request, response) {
-  const url = `https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=${process.env.TRAILS_API_KEY}`;
+  const url = `https://www.hikingproject.com/data/get-trails?lat=${request.query.data.latitude}&lon=${request.query.data.longitude}&maxDistance=10&key=${process.env.TRAILS_API_KEY}`;
 
   superagent.get(url)
     .then(result => {
+      // console.log(result.body.trails);
       const trails = result.body.trails.map(path => {
-        console.log(path);
         return new Trails(path);
       });
       response.send(trails);
@@ -132,9 +143,24 @@ function getTrails(request, response) {
     .catch(error => handleError(error, response));
 }
 
+function getMeetup(request, response) {
+  const url = `https://api.meetup.com/find/upcoming_events?key=${process.env.MEETUP_API_KEY}&lat=${request.query.data.latitude}&lon=${request.query.data.longitude}`;
+
+  superagent.get(url)
+    .then(result => {
+      const meetups = result.body.events.map(event => {
+        
+        // console.log(event.venue);
+        return new Meetup(event);
+      })
+      response.send(meetups);
+
+    })
+    .catch(error => handleError(error, response));
+}
 
 function getYelp(request, response) {
-  const url = `https://api.yelp.com/v3/businesses/search?location=${request.query.data.search_query}`
+  const url = `https://api.yelp.com/v3/businesses/search?location=${request.query.data.search_query}`;
 
   superagent.get(url)
     .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
